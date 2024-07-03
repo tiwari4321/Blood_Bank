ba279ee77389d46198cb258030c5d7d32dfb2a27
Abhinav Tiwari <abhinavtiwari4320@gmail.com>
2024-07-03 19:58:23 +0530

    Login Action and Reducer

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,12 @@ import {Routes,Route} from 'react-router-dom'
 import Homepage from './pages/Homepage';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
     <>
+    <ToastContainer/>
       <Routes>
         <Route path = '/' element = {<Homepage/>}/>
         <Route path = '/login' element= {<Login/>}/>
--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,10 +1,14 @@
+import { userLogin } from "../redux/features/auth/authActions";
+import store from "../redux/store"
+
+
 export const handleLogin = (e, email, password, role) =>{
     e.preventDefault()
     try {
         if(!role || !email || !password){
             return alert('Please Provide All field');
         }
-        console.log('login ',e, email, password, role)
+        store.dispatch(userLogin({email, password, role}))
     } catch (error) {
         console.log(error)
     }
