9376a3804b8e5ddd72b0b666ee3681785c6f139b
Shubham Shukla <shuklashub342003@gmail.com>
2024-07-05 10:38:28 +0530

    loading state and error handling

---
+++ client/src/components/shared/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header';
+
+const Layout = ({ children }) => {
+    return (
+        <>
+            <div>
+                <div className="header"><Header /></div>
+                <div className="content">{children}</div>
+            </div>
+        </>
+
+    )
+}
+
+export default Layout;
